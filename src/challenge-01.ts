1c9b7b4f9c9b2791ba18f5af5bf9cb79912e1a35
Ezequiel Dapena <ezequieldapena@gmail.com>
2024-12-03 07:46:31 -0300

    Day 3 of adventJs 2024

--- src/challenge-01.ts
+++ src/challenge-01.ts
@@ -1,3 +1,8 @@
+// Santa Claus 🎅 has received a list of magical numbers representing gifts 🎁, but some of them are duplicated and must be removed to avoid confusion. Additionally, the gifts must be sorted in ascending order before being delivered to the elves.
+
+// Your task is to write a function that receives a list of integers (which may include duplicates) and returns a new list without duplicates, sorted in ascending order.Santa Claus 🎅 has received a list of magical numbers representing gifts 🎁, but some of them are duplicated and must be removed to avoid confusion. Additionally, the gifts must be sorted in ascending order before being delivered to the elves.
+// Your task is to write a function that receives a list of integers (which may include duplicates) and returns a new list without duplicates, sorted in ascending order.
+
 function prepareGifts(gifts: number[]): number[] {
   const uniqueGifts = Array.from(new Set(gifts)).sort((a, b) => a - b);
   return uniqueGifts;
