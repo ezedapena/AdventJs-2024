40ef62b1a4a86d12363627c2169ed949624ebf18
Ezequiel Dapena <ezequieldapena@gmail.com>
2024-12-09 08:42:34 -0300

    Day 9 of AdventJs 2024 1/5 starts

---
+++ src/challenge-09.ts
@@ -0,0 +1,84 @@
+/*
+
+Los elfos están jugando con un tren 🚂 mágico que transporta regalos. Este tren se mueve en un tablero representado por un array de strings.
+
+El tren está compuesto por una locomotora (@), seguida de sus vagones (o), y debe recoger frutas mágicas (*) que le sirve de combustible. El movimiento del tren sigue las siguientes reglas:
+
+Recibirás dos parámetros board y mov.
+
+board es un array de strings que representa el tablero:
+
+@ es la locomotora del tren.
+o son los vagones del tren.
+* es una fruta mágica.
+· son espacios vacíos.
+mov es un string que indica el próximo movimiento del tren desde la cabeza del tren @:
+
+'L': izquierda
+'R': derecha
+'U': arriba
+'D': abajo.
+Con esta información, debes devolver una cadena de texto:
+
+'crash': Si el tren choca contra los bordes del tablero o contra sí mismo.
+'eat': Si el tren recoge una fruta mágica (*).
+'none': Si avanza sin chocar ni recoger ninguna fruta mágica
+
+*/
+
+type Space = '·' | '@' | '*' | 'o'
+type Board = Space[][]
+type Movement = 'U' | 'D' | 'R' | 'L'
+type Result = 'none' | 'crash' | 'eat'
+
+function moveTrain(board: Board, mov: Movement): Result {
+    function findInteraction(space: Space | null): Result {
+        if (!!space) {
+            switch (space) {
+                case '·': return 'none'
+                case '*': return 'eat'
+                case 'o': return 'crash'
+            }
+        }
+
+        return 'crash';
+    }
+
+    const engineRow = board.findIndex(row => row.includes('@'));
+    const engineColumn = board[engineRow].indexOf('@');
+
+    let movSpace: Space | null = null;
+
+    switch (mov) {
+        case "U": movSpace = board[engineRow - 1]?.[engineColumn] ?? null; break;
+        case "D": movSpace = board[engineRow + 1]?.[engineColumn] ?? null; break;
+        case "R": movSpace = board[engineRow]?.[engineColumn + 1] ?? null; break;
+        case "L": movSpace = board[engineRow]?.[engineColumn - 1] ?? null; break;
+    }
+
+    return findInteraction(movSpace);
+}
+
+const board: Board = [
+    ['·', '·', '·', '·', '·'],
+    ['*', '·', '·', '·', '·'],
+    ['@', '·', '·', '·', '·'],
+    ['o', '·', '·', '·', '·'],
+    ['o', '·', '·', '·', '·'],
+];
+
+console.log(moveTrain(board, 'U'))
+// ➞ 'eat'
+// Porque el tren se mueve hacia arriba y encuentra una fruta mágica
+
+console.log(moveTrain(board, 'D'))
+// ➞ 'crash'
+// El tren se mueve hacia abajo y la cabeza se choca consigo mismo
+
+console.log(moveTrain(board, 'L'))
+// ➞ 'crash'
+// El tren se mueve a la izquierda y se choca contra la pared
+
+console.log(moveTrain(board, 'R'))
+// ➞ 'none'
+// El tren se mueve hacia derecha y hay un espacio vacío en la derecha
